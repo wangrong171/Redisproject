606f0d07e20a706273623bfff9d9663516fa47c0
tadashi-fiona <13621964413@139.com>
2020-06-22 16:20:11 +0800

    add class1

---
+++ ConnectionWatcher/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionWatcher
+{
+    class Class1
+    {
+
+        public void print()
+        {
+            Console.Write("hello world");
+
+        }
+       
+    }
+}
